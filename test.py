23d104945f37394cd00eb9fe7d186f3d75b225b1
scyoon <scyoon@stealien.com>
2021-02-23 13:07:03 +0900

    [New] 프로토타입 추가

---
+++ test.py
@@ -0,0 +1,55 @@
+import json
+import time
+import pytz
+import pprint
+import requests
+
+from datetime import datetime, timezone, timedelta
+from http.cookiejar import http2time
+
+
+def get_server_time(session: requests.Session):
+    local_timezone = pytz.timezone('Asia/Seoul')
+
+    url = 'http://sugang.khu.ac.kr/blank.html?fake=1'
+    resp = session.get(url, timeout=10)
+
+    # Fri, 22 Jan 2021 04:00:01 GMT
+    raw_server_time = resp.headers['Date']
+    server_time = datetime.utcfromtimestamp(http2time(raw_server_time))\
+        .replace(tzinfo=timezone.utc)\
+        .astimezone(tz=local_timezone)
+
+    return server_time
+
+
+def get_accurate_server_time(session: requests.Session):
+    """
+    서버시간과의 오차 0.01초로 줄이는 함수
+    """
+    previous_time = get_server_time(session)
+    while True:
+        time.sleep(0.001)
+        current_time = get_server_time(session)
+
+        if previous_time != current_time:
+            return current_time
+        else:
+            previous_time = current_time
+
+
+def main():
+    delta = 0.1
+    session = requests.Session()
+    server_time = get_accurate_server_time(session)
+    
+    execute_seconds = 0
+    for _ in range(100000000000000):
+        guessed_server_time = server_time + timedelta(seconds=execute_seconds)
+        print(f'[ INFO ] guessed server time : {guessed_server_time}')
+        time.sleep(delta)
+        execute_seconds += delta
+
+
+if __name__ == '__main__':
+    main()
